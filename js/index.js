6516455a63eab5b0d0ba032ddc37b454f486c002
Philip Boen <philipboen001@gmail.com>
2023-12-15 21:48:40 +0300

    added the toggle functionality

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
 const jobStatusText = document.querySelectorAll('.job_status_text');
 const jobStatus = document.querySelectorAll('.job_status');
 
+const eyeHide = document.querySelector('.eye_hide');
+const eyeVisible = document.querySelector('.eye_visible');
+const password = document.querySelector('#password');
+
+
 for (let i = 0; i < jobStatusText.length; i++) {
     if (jobStatusText[i].innerText === "Part Time") {
         jobStatus[i].style.background = "#DFCD8E";
     }
 }
+
+eyeHide.addEventListener('click', () => {
+    eyeHide.style.display = "none";
+    eyeVisible.style.display = "block";
+    password.type = "text";
+});
+
+eyeVisible.addEventListener('click', () => {
+    eyeVisible.style.display = "none";
+    eyeHide.style.display = "block";
+    password.type = "password";
+});
