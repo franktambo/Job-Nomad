106be30b25f554735a78d2e7330c0c07805f1565
Philip Boen <philipboen001@gmail.com>
2023-12-16 18:50:01 +0300

    added signup page

--- js/index.js
+++ js/index.js
@@ -5,6 +5,10 @@ const eyeHide = document.querySelector('.eye_hide');
 const eyeVisible = document.querySelector('.eye_visible');
 const password = document.querySelector('#password');
 
+const eye_hide_confirm = document.querySelector('.eye_hide_confirm');
+const eye_visible_confirm = document.querySelector('.eye_visible_confirm');
+const confirmPass = document.querySelector('#confirm_pass');
+
 
 for (let i = 0; i < jobStatusText.length; i++) {
     if (jobStatusText[i].innerText === "Part Time") {
@@ -16,10 +20,24 @@ eyeHide.addEventListener('click', () => {
     eyeHide.style.display = "none";
     eyeVisible.style.display = "block";
     password.type = "text";
+    
 });
 
 eyeVisible.addEventListener('click', () => {
     eyeVisible.style.display = "none";
     eyeHide.style.display = "block";
     password.type = "password";
+});
+
+eye_hide_confirm.addEventListener('click', () => {
+    eye_hide_confirm.style.display = "none";
+    eye_visible_confirm.style.display = "block";
+    confirmPass.type = "text";
+    
+});
+
+eye_visible_confirm.addEventListener('click', () => {    
+    eye_visible_confirm.style.display = "none";
+    eye_hide_confirm.style.display = "block";
+    confirmPass.type = "password";
 });
